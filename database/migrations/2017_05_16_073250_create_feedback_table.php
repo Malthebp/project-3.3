577bf6bf1fa8d141f60dcdc39c166d6e79d26368
Malthe Petersen <malthe@maggerit.dk>
2017-05-16 10:06:09 +0200

    Created all migrations, controllers + models - Malthe

--- database/migrations/2017_05_16_073250_create_feedback_table.php
+++ database/migrations/2017_05_16_073250_create_feedback_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateFeedbackTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('feedback', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('balance');
-            $table->string('image');
-            $table->integer('role_id')->unsigned();
-            $table->rememberToken();
+            $table->text('comment');
+            $table->integer('user_id')->unsigned();
+            $table->integer('lecture_id')->unsigned();
             $table->timestamps();
         });
     }
@@ -33,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('feedback');
     }
 }
